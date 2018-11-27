211e9dd8b0785c158732e82c6349df8e8583bfb1
Alexander G. Zimmerman <alexander.zimmerman@rwth-aachen.de>
2018-11-27 10:55:58 +0100

    Removed fluff from setup

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # python-package
-This is a minimal example of a Python 3 package structure.
+This is a minimal example of a Python 3 package containing subpackages that can be installed with `pip`.
--- setup.py
+++ setup.py
@@ -1,25 +1,8 @@
 import setuptools
 
 
-with open("README.md") as f:
-
-    readme = f.read()
-
-with open("LICENSE.txt") as f:
-
-    license = f.read()
-
-packages = setuptools.find_packages(exclude = ("tests",))
-
-print("Found packages " + str(packages))
-
 setuptools.setup(
     name = "package",
-    version = "0.1.0a0",
-    packages = packages,
-    description = "A minimal example of a Python package",
-    long_description = readme,
-    license = license,
-    author = "Alexander G. Zimmerman",
-    author_email = "zimmerman@aices.rwth-aachen.de",
+    version = "0.1.0a1",
+    packages = setuptools.find_packages(),
 )
